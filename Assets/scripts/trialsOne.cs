8db71f929a95dacd48545a4232f479f08c28b68b
E9ex <egecansar@gmail.com>
2023-05-27 01:54:28 +0300

    dotweenexamples

--- Assets/scripts/trialsOne.cs
+++ Assets/scripts/trialsOne.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class trialsOne : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cube;
     void Start()
     {
         
@@ -13,6 +14,16 @@ public class trialsOne : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //cube.DOMove(Vector3.zero, 1f);// hedeflenen nokta, ne kadar sürede gerçekleşmesini yazıyoruz..
+          //  cube.DOMoveX(3f, 1f);// bu da x yönünde sadece gitmesi...
+          //cube.DORotate(new Vector3(0, 0, -180),1f); dönmesi için space'e bastigimizda dönmesini istiyorsak sürekli
+          // cube.dorotate(cube.localrotation.eulerangles+newvector(0,0,-180),1f); 
+          //cube.DOScale(new Vector3(3, 3, 3), 1f);
+          //cube.DOScale(vector.zero),1f).From(); scale i sıfırdan başlatir ve oldugu noktaya getirir.(From*)
+          //easing.net
+         // cube.DOMoveX(0f, 1f).SetLoops(-1,LoopType.Yoyo);
+        }
     }
 }
