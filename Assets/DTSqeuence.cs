377e15d8186f4d4db64b1674bc2a9a54fcdc68d1
E9ex <egecansar@gmail.com>
2023-05-29 19:09:35 +0300

    dtseq

--- Assets/DTSqeuence.cs
+++ Assets/DTSqeuence.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DTSqeuence : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Transform> objects;
     void Start()
     {
-        
-    }
+        // objects[0].DOMove(objects[0].transform.position + objects[0].transform.forward * 10, 1).OnComplete(() =>
+        // {
+        //     objects[1].DOMove(objects[1].transform.position + objects[1].transform.right * 10, 1)
+        //         .OnComplete(() =>
+        //         {
+        //             objects[2].DOMove(objects[2].transform.position + objects[2].transform.right * 10, 1);
+        //
+        //         });
+        // }); sqeuencesiz hali.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var sequence = DOTween.Sequence();
+        foreach (Transform pp in objects)
+        {
+            sequence.Append(pp.DOMove(pp.transform.position + pp.transform.forward * 10, 1));
+        }
+        //sequence.SetLoops(-1); isteğe bagli.
+
+
     }
 }
+
